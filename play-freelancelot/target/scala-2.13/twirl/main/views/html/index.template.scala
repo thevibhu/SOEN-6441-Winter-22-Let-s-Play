7fa4ffe6601c1bb3ebaf248d676841c908afdbd6
thevibhu <thevaibhavverma@gmail.com>
2022-02-28 21:27:23 -0500

    modifying gitignore

---
+++ play-freelancelot/target/scala-2.13/twirl/main/views/html/index.template.scala
@@ -0,0 +1,59 @@
+
+package views.html
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+import scala.jdk.CollectionConverters._
+
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*1.4*/("""
+
+"""),_display_(/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+  """),format.raw/*4.3*/("""<h1>Welcome to Play!</h1>
+""")))}),format.raw/*5.2*/("""
+"""))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  SOURCE: app/views/index.scala.html
+                  HASH: adcb29c5791d4edf4d22588588285779a8bdbe5d
+                  MATRIX: 900->1|996->3|1026->8|1057->31|1096->33|1126->37|1183->65
+                  LINES: 27->1|32->1|34->3|34->3|34->3|35->4|36->5
+                  -- GENERATED --
+              */
+          
