881b934fdc9f232bc90456f7a4c15c313609ff4d
gagan <gaganpandher04@gmail.com>
2022-04-13 19:25:53 -0400

    some fix

--- play-freelancelot/test/dao/UserProjectDisplayTest.java
+++ play-freelancelot/test/dao/UserProjectDisplayTest.java
@@ -26,9 +26,11 @@ public class UserProjectDisplayTest {
 	@Test
 	public void test() {
 		UserProjectDisplay proj = new UserProjectDisplay();
+		proj.setTime_submitted(0);
 		proj.setTitle("title");
 		proj.setOwner_id(1);
 		proj.setType("SomeType");
+		assertEquals("December 31, 1969", proj.getTime_submitted());
 		assertEquals(1, proj.getOwner_id());
 		assertEquals("SomeType", proj.getType());
 		assertEquals("title", proj.getTitle());
@@ -40,7 +42,8 @@ public class UserProjectDisplayTest {
 	 */
 	@Test
 	public void testConstructor() {
-		UserProjectDisplay p = new UserProjectDisplay("title", "SomeType", 1);
+		UserProjectDisplay p = new UserProjectDisplay("title", 0, "SomeType", 1);
+		assertEquals("December 31, 1969", p.getTime_submitted());
 		assertEquals(1, p.getOwner_id());
 		assertEquals("SomeType", p.getType());
 		assertEquals("title", p.getTitle());
